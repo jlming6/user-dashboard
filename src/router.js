a15071caf276e4757485fd339a63bbdec30ef615
Mingkui Liu <liumingkui@gmail.com>
2017-09-14 22:10:14 -0700

    English version

--- src/router.js
+++ src/router.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Router, Switch, Route } from 'dva/router';
 import dynamic from 'dva/dynamic';
 
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+
 function RouterConfig({ history, app }) {
   const IndexPage = dynamic({
     app,
@@ -17,12 +20,14 @@ function RouterConfig({ history, app }) {
   });
 
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={IndexPage} />
-        <Route exact path="/users" component={Users} />
-      </Switch>
-    </Router>
+    <LocaleProvider locale={enUS}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/users" component={Users} />
+        </Switch>
+      </Router>
+    </LocaleProvider>
   );
 }
 
