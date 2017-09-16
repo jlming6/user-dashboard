8089424101f6d368fd2533a22a345b29dbc7e19f
Mingkui Liu <liumingkui@gmail.com>
2017-09-15 21:01:15 -0700

    Fix the submenu problems.

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,17 +1,45 @@
 import React from 'react';
+
+import { Layout, Menu, Icon } from 'antd';
+import { Link } from 'dva/router';
+
 import styles from './MainLayout.css';
-import Header from './Header';
+
+const { Header, Content } = Layout;
+
+const SubMenu = Menu.SubMenu;
 
 function MainLayout({ children, location }) {
   return (
-    <div className={styles.normal}>
-      <Header location={location} />
-      <div className={styles.content}>
-        <div className={styles.main}>
-          {children}
+    <Layout className={styles.normal}>
+      <Header style={{ padding: '0 0px' }}>
+        <div className="logo" />
+        <Menu
+          selectedKeys={[location.pathname]}
+          mode="horizontal"
+          theme="dark"
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="/">
+            <Link to="/"><Icon type="home" />Messaging Platform</Link>
+          </Menu.Item>
+          <Menu.Item key="/users">
+            <Link to="/users"><Icon type="bars" />Users</Link>
+          </Menu.Item>
+          <SubMenu key="/cms" title={<span><Icon type="appstore" /><span>CMS</span></span>}>
+            <Menu.Item key="/campaigns">Campaigns</Menu.Item>
+            <Menu.Item key="/audience_segments">Audience Segments</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Header>
+      <Content>
+        <div className={styles.content}>
+          <div className={styles.main}>
+            {children}
+          </div>
         </div>
-      </div>
-    </div>
+      </Content>
+    </Layout>
   );
 }
 
